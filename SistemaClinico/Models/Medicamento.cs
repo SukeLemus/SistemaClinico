b623842ec0d876a4025cbb88c435c8ecb9313c40
HAN MIN HU GONZALEZ <2561012015@mail.utec.edu.sv>
2021-01-08 16:30:01 -0600

    y

--- SistemaClinico/Models/Medicamento.cs
+++ SistemaClinico/Models/Medicamento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,17 @@ namespace SistemaClinico.Models
     public class Medicamento
     {
         public int id { get; set; }
+        [Required]
         public String NOMBRE_MEDICAMENTO { get; set; }
-
+        [Required]
         public String DESCRIPCION_MEDICAMENTO { get; set; }
-
+        [Required]
         public int CANTIDAD { get; set; }
+        [Required]
         public String PRESENTACION { get; set; }
+        [Required]
         public double PRECIO_UNITARIO { get; set; }
+        [Required]
         public String ESTADO { get; set; }
 
     }
